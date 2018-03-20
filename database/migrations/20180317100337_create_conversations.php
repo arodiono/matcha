6e2680fb662c1ffd533c9a58094b3eb90ffc5576
oklymeno <klymenok.a@gmail.com>
2018-03-20 23:40:47 +0200

    59e78e9f37f4153bab3a853f Chat 14h 41m

---
+++ database/migrations/20180317100337_create_conversations.php
@@ -0,0 +1,43 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateConversations extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $messages = $this->table('conversations');
+        $messages
+            ->addColumn('user_id_1', 'integer', ['limit' => 11])
+            ->addColumn('user_id_2', 'integer', ['limit' => 11])
+            ->addColumn('created_at', 'timestamp')
+            ->addColumn('updated_at', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'update' => 'CURRENT_TIMESTAMP',
+                'null' => TRUE
+            ])
+            ->create();
+    }
+}
