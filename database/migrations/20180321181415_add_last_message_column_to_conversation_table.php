aecd65700b8b77ad90761c19bd9a5c98831e6709
oklymeno <klymenok.a@gmail.com>
2018-03-21 21:30:36 +0200

    lost migration added

---
+++ database/migrations/20180321181415_add_last_message_column_to_conversation_table.php
@@ -0,0 +1,34 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class AddLastMessageColumnToConversationTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $conversations = $this->table('conversations');
+        $conversations->addColumn('last_message', 'string', ['default' => ''])->save();
+    }
+}
