e2075086aefa5aa22568bc43fdbe66bf24d7c2da
Andrii Rodionov <arodiono@student.unit.ua>
2018-06-08 18:48:19 +0300

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,15 +1,15 @@
 <?php
 
 define('DB_DRIVER', 'mysql');
-define('DB_HOST', '35.198.137.227');
-define('DB_USER', 'test');
-define('DB_PASS', 'test');
-define('DB_NAME', 'matcha');
-define('DB_CHARSET', 'utf8mb4');
-define('DB_COLLATION', 'utf8mb4_unicode_ci');
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+define('DB_CHARSET', '');
+define('DB_COLLATION', '');
 define('DB_PORT', '3306');
-define('INTRA_UID', '0b140691892f202e371a2e6600153ffe382b3a9d35aa87266e97af33eb299a00');
-define('INTRA_SECRET', '00989f3319d881f0b50af7aa9c0fecebe5cce4690595e8a821fa22086861cf21');
+define('INTRA_UID', '');
+define('INTRA_SECRET', '');
 
-define('MAIL_PASS', 'arodiono-oklymeno-matcha');
-define('GOOGLE_API_KEY', 'AIzaSyC1hk1YWs9aHsxzRKXuw9yHTQw0DV_1bao');
+define('MAIL_PASS', '');
+define('GOOGLE_API_KEY', '');
